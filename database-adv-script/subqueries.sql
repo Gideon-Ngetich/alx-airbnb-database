9bf491e95d04a0483050a2060b5f13448fba3a0f
Gideon-Ngetich <sunbird082003@gmail.com>
2025-07-08 16:43:16 +0300

    Sub Query correction

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,17 +1,13 @@
 SELECT 
     p.property_id,
     p.name,
-    p.location,
-    review_stats.review_count,
-    review_stats.avg_rating
+    p.location
 FROM properties p
-JOIN (
+WHERE p.property_id IN (
     SELECT 
         r.property_id,
         COUNT(*) AS review_count,
-        AVG(r.rating) AS avg_rating
     FROM reviews r
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4.0
-) AS review_stats
-ON p.property_id = review_stats.property_id;
+);
