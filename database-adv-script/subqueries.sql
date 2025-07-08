592ede7795e7157c3f4aa956ff05facb183916b8
Gideon-Ngetich <sunbird082003@gmail.com>
2025-07-08 16:46:50 +0300

    Sub Query correction

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,13 +1,25 @@
+-- 1. Find all properties where the average rating is greater than 4.0 using a subquery
 SELECT 
     p.property_id,
     p.name,
     p.location
 FROM properties p
 WHERE p.property_id IN (
-    SELECT 
-        r.property_id,
-        COUNT(*) AS review_count,
+    SELECT r.property_id
     FROM reviews r
     GROUP BY r.property_id
     HAVING AVG(r.rating) > 4.0
 );
+
+-- 2. Find users who have made more than 3 bookings using a correlated subquery
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
